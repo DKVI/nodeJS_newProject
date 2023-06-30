d3e772c0d6639dfca00ef8ad62ec6a77fd76c8ea
DKVI <92564346+DKVI@users.noreply.github.com>
2023-06-30 14:51:49 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@
 
 const express = require("express");
 const morgan = require("morgan");
+require("dotenv").config();
 const handlebars = require("express-handlebars").engine;
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+console.log("env: ", process.env.PORT);
 const path = require("path");
 app.use(express.static(path.join(__dirname, "public")));
 app.engine("handlebars", handlebars());
@@ -17,4 +19,4 @@ app.get("/news", (req, res) => {
   res.render("news");
 });
 
-app.listen(3000, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
