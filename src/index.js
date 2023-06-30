5a6efdf3367790dd084a99620b5dbd6f77f40ea3
DKVI <92564346+DKVI@users.noreply.github.com>
2023-06-30 11:00:50 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -6,12 +6,10 @@ const handlebars = require("express-handlebars").engine;
 const app = express();
 const port = 3000;
 const path = require("path");
-
+app.use(express.static(path.join(__dirname, "public")));
 app.engine("handlebars", handlebars());
 app.set("view engine", "handlebars");
-
 app.set("views", path.join(__dirname, "resources", "views"));
-
 app.get("/", (req, res) => {
   res.render("home");
 });
