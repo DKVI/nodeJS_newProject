124a7bba368d835b126fb4af3ef953dd71cd22f3
DKVI <92564346+DKVI@users.noreply.github.com>
2023-06-30 10:22:03 +0700

    commit

--- index.js
+++
@@ -1,9 +0,0 @@
-/** @format */
-
-const express = require("express");
-const app = express();
-const port = 3000;
-
-app.use("/trangchu", (req, res) => res.send("Hello World!"));
-
-app.listen(3000, () => console.log(`Example app listening on port ${port}!`));
---
+++ src/index.js
@@ -0,0 +1,22 @@
+/** @format */
+
+const express = require("express");
+const morgan = require("morgan");
+const handlebars = require("express-handlebars").engine;
+const app = express();
+const port = 3000;
+const path = require("path");
+
+app.engine("handlebars", handlebars());
+app.set("view engine", "handlebars");
+
+app.set("views", path.join(__dirname, "resources", "views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/news", (req, res) => {
+  res.render("news");
+});
+
+app.listen(3000, () => console.log(`Example app listening on port ${port}!`));
