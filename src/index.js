b39a7814da6054725da222ae58e4a4d1ad5034e3
DKVI <92564346+DKVI@users.noreply.github.com>
2023-07-03 11:52:28 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -9,14 +9,32 @@ const port = process.env.PORT || 3000;
 console.log("env: ", process.env.PORT);
 const path = require("path");
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.engine("handlebars", handlebars());
+
 app.set("view engine", "handlebars");
+
 app.set("views", path.join(__dirname, "resources", "views"));
+
 app.get("/", (req, res) => {
+  console.log(res.statusCode);
   res.render("home");
 });
+
 app.get("/news", (req, res) => {
   res.render("news");
+});
+
+app.get("/search", (req, res) => {
+  res.render("search");
+});
+
+app.post("/search", (req, res) => {
+  console.log(req.body);
+  res.send("");
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
