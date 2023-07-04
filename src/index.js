62463a37012abf261c50d6a71c70de5dae36cb2f
DKVI <92564346+DKVI@users.noreply.github.com>
2023-07-04 23:18:44 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -1,40 +1,28 @@
 /** @format */
 
-const express = require("express");
-const morgan = require("morgan");
-require("dotenv").config();
-const handlebars = require("express-handlebars").engine;
+const express = require('express');
+const morgan = require('morgan');
+require('dotenv').config();
+const handlebars = require('express-handlebars').engine;
 const app = express();
 const port = process.env.PORT || 3000;
-console.log("env: ", process.env.PORT);
-const path = require("path");
-app.use(express.static(path.join(__dirname, "public")));
+console.log('env: ', process.env.PORT);
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+console.log(__dirname);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.engine("handlebars", handlebars());
+app.engine('handlebars', handlebars());
 
-app.set("view engine", "handlebars");
+app.set('view engine', 'handlebars');
 
-app.set("views", path.join(__dirname, "resources", "views"));
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
-app.get("/", (req, res) => {
-  console.log(res.statusCode);
-  res.render("home");
-});
+const route = require('./routes');
 
-app.get("/news", (req, res) => {
-  res.render("news");
-});
-
-app.get("/search", (req, res) => {
-  res.render("search");
-});
-
-app.post("/search", (req, res) => {
-  console.log(req.body);
-  res.send("");
-});
+// Route init
+route(app);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+const newsRouter = require('./news.router');
+const homeRouter = require('./home.router');
+function route(app) {
+  app.use('/', homeRouter);
+  app.use('/homes', homeRouter);
+
+  app.use('/news', newsRouter);
+
+  app.get('/search', (req, res) => {
+    res.render('search');
+  });
+
+  app.post('/search', (req, res) => {
+    console.log(req.body);
+    res.send('');
+  });
+}
+
+module.exports = route;
